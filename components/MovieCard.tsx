095b72121dc70dd75b12f65719ee73ea8504179f
shareef <shareef@sajilni.com>
2023-12-16 17:42:33 +0300

    fix to Watch component of trending now

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -26,7 +26,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
           <div className="flex flex-row items-center gap-3">
             <div
-              onClick={() => router.push(`/watch/movie/${data?.id}`)}
+              onClick={() => router.push(`/watch/${data?.id}`)}
               className="bg-white rounded-full cursor-pointer w-6 h-6 lg:w-10 lg:h-10 flex justify-center transition hover:bg-neutral-300 items-center"
             >
               <BsFillPlayFill size={30} />
