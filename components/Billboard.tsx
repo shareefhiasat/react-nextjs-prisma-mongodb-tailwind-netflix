037f415f453fea868ff3fa9b27de79285ccc90a3
shareef <shareef@sajilni.com>
2023-12-16 17:28:33 +0300

    Add play button

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,6 +1,7 @@
 import useBillboard from "@/hooks/useBillboard";
 import React from "react";
 import { CiCircleInfo } from "react-icons/ci";
+import PlayButton from "./PlayButton";
 
 const Billboard = () => {
   const { data } = useBillboard();
@@ -26,6 +27,7 @@ const Billboard = () => {
           {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <PlayButton movieId={data?.id} />
           <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 flex flex-row items-center hover:bg-opacity-20 transition font-semibold w-auto sm:text-sm md:text-base text-xs">
             <CiCircleInfo className="mr-1" />
             More Info
